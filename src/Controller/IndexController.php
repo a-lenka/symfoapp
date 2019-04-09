63cdd3c80098fe28ae178a0c1e632005f11ba5b0
lena <rikhnavskaja.elena@gmail.com>
2019-04-09 14:24:58 +0200

    Add simple Controller

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/")
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
+}
