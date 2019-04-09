a619c6ba768230ccd211c514f89fd3d4e98f19b2
lena <rikhnavskaja.elena@gmail.com>
2019-04-09 12:19:18 +0200

    Install Symfony

---
+++ tests/_support/Helper/Acceptance.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Helper;
+
+use Codeception\Module;
+
+/**
+ * Here you can define custom actions.
+ * All public methods will be available in $I
+ *
+ * Class Acceptance
+ * @package App\Tests\Helper
+ */
+class Acceptance extends Module
+{
+
+}
---
+++ tests/_support/Helper/Unit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Helper;
+
+use Codeception\Module;
+
+/**
+ * Here you can define custom actions.
+ * All public methods will be available in $I
+ *
+ * Class Unit
+ * @package App\Tests\Helper
+ */
+class Unit extends Module
+{
+
+}
