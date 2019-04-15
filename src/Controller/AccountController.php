b75ed54dec2ae40860d7355bac9611a9eb8504e5
lena <rikhnavskaja.elena@gmail.com>
2019-04-15 15:41:56 +0200

    Add simple Account page

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Manage User's account page
+ *
+ * Class AccountController
+ * @package App\Controller
+ */
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/{_locale}/account",
+     *     name="account",
+     *     methods="GET",
+     *     defaults={"_locale"="%default_locale%"},
+     *     requirements={"_locale": "%app_locales%"},
+     * )
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig');
+    }
+}
