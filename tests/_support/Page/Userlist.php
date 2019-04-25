a5fb95fc41b68f8c072bb55490f5fe0311f7dc85
lena <rikhnavskaja.elena@gmail.com>
2019-04-25 17:03:00 +0200

    Add tests for User Update action

--- tests/_support/Page/Userlist.php
+++ tests/_support/Page/Userlist.php
@@ -26,4 +26,16 @@ class Userlist
     public static $links = [
         'topmenu_link_text' => 'Users'
     ];
+
+    /** @var array $form - Form data */
+    public static $form = [
+        'email_field_text'    => 'User email',
+        'roles_field_text'    => 'User roles',
+        'password_field_text' => 'User password',
+        'submit_button_tag'   => 'button',
+        'admin_option'        => 'Admin',
+        'root_option'         => 'Root',
+        'user_option'         => 'User',
+        'select_tag'          => 'select',
+    ];
 }
