31a1e48fcd25b0a7c461686d2f0b42d9ad01104d
lena <rikhnavskaja.elena@gmail.com>
2019-04-26 09:23:03 +0200

    Add Functional tests for sorting Users by Email

--- tests/functional/Controller/User/UserShowListCest.php
+++ tests/functional/Controller/User/UserShowListCest.php
@@ -60,6 +60,50 @@ class UserShowListCest
 
 
     /**
+     * @param FunctionalTester $I
+     */
+    public function testSortUsersByEmail(FunctionalTester $I): void
+    {
+        $vars = self::getVars();
+        $I->amOnPage($vars['home_url']);
+
+        $I->amGoingTo('save new Users to the Database');
+        $first = new User();
+        $I->persistEntity($first, [
+            'email'    => 'aaa@mail.ru',
+            'password' => 'bla-bla-bla',
+            'roles'    => ['ROLE_TEST']
+        ]);
+        $I->seeInRepository(User::class, ['email' => 'aaa@mail.ru']);
+
+
+        $third = new User();
+        $I->persistEntity($third, [
+            'email'    => 'zzz@mail.ru',
+            'password' => 'bla-bla-bla',
+            'roles'    => ['ROLE_TEST']
+        ]);
+        $I->seeInRepository(User::class, ['email' => 'zzz@mail.ru']);
+
+
+        $I->am('Tester');
+        $I->amGoingTo('see all saved users in the Userlist');
+        $I->click($vars['user_link']);
+        $I->seeCurrentUrlEquals($vars['userlist_url']);
+        $I->see('aaa@mail.ru');
+        $I->see('zzz@mail.ru');
+
+        $I->amGoingTo('sort Users with asc order');
+        $I->click("a[href=\"".$vars['email_desc']."\"]");
+        $I->see('zzz@mail.ru', 'table tbody tr:first-child');
+
+        $I->amGoingTo('sort Users with asc order');
+        $I->click("a[href=\"".$vars['email_asc']."\"]");
+        $I->see('aaa@mail.ru', 'table tbody tr:first-child');
+    }
+
+
+    /**
      * @return array
      */
     private static function getVars(): array
@@ -70,6 +114,10 @@ class UserShowListCest
             'header_text' => Userlist::$header['text'],
             'header_tag'  => Userlist::$header['tag'],
             'user_link'   => Userlist::$links['topmenu_link_text'],
+            'email_asc'   => Userlist::$sort['email_asc'],
+            'email_desc'  => Userlist::$sort['email_desc'],
+            'roles_asc'   => Userlist::$sort['roles_asc'],
+            'roles_desc'  => Userlist::$sort['roles_desc'],
         ];
     }
 }
