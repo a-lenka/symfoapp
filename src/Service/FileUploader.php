a50f34c9b8ba59353ed82eb3e4cb911e2c3655a9
lena <rikhnavskaja.elena@gmail.com>
2019-05-14 11:42:48 +0200

    Use filename as prefix on file uploading

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -35,11 +35,19 @@ class FileUploader
      *
      * @return string
      */
-    public function uploadUserAvatar(?UploadedFile $uploadedFile): string
+    final public function uploadUserAvatar(?UploadedFile $uploadedFile): string
     {
         if ($uploadedFile) {
             // TODO: $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid(mt_rand().'.'.$uploadedFile->guessExtension();
-            $newFileName = uniqid('', false).'.'.$uploadedFile->guessExtension();
+            // Old filename
+            $oldFileName = $uploadedFile->getClientOriginalName();
+            $trimmed     = pathinfo($oldFileName, PATHINFO_FILENAME);
+
+            // New filename
+            $unique      = uniqid('', false).'.'.$uploadedFile->guessExtension();
+            $newFileName = $trimmed.'_'.$unique;
+
+            // Move
             $destination = $this->uploadsPath.'/avatars';
             $uploadedFile->move($destination, $newFileName);
 
